f4e5977a88643261ef734063b9a85d52d01901f3
mutsuki <mutsuki@momo86.net>
2019-04-29 17:10:58 +0900

    update : add print_matrix_16x16

--- src/utils.hpp
+++ src/utils.hpp
@@ -22,6 +22,21 @@ __device__ __host__ inline void print_matrix(const T* const ptr, std::size_t m, 
 	}
 }
 template <class T>
+__device__ __host__ inline void print_matrix_16x16(const T* const ptr, std::size_t m, std::size_t n, const char *name = nullptr){
+	if(name != nullptr) printf("%s = \n", name);
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < n; j++){
+			const auto val = cutf::cuda::type::cast<float>(ptr[j * 16 + i]);
+			if(val < 0.0f){
+				printf("%.5f ", val);
+			}else{
+				printf(" %.5f ", val);
+			}
+		}
+		printf("\n");
+	}
+}
+template <class T>
 __device__ __host__ inline void print_matrix_diag(const T* const ptr, std::size_t n, const char *name = nullptr){
 	if(name != nullptr) printf("%s = \n", name);
 	for(int j = 0; j < n; j++){
