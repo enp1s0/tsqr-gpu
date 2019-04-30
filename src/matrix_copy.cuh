b0dd7edd047eb74b099313efddf5e2b2c831d7d9
mutsuki <mutsuki@momo86.net>
2019-04-30 12:46:05 +0900

    update : add g2s32x16, s2g32x16

--- src/matrix_copy.cuh
+++ src/matrix_copy.cuh
@@ -49,6 +49,44 @@ __device__ inline void s2g16x16(
 		global_ptr[global_index] = shared_ptr[shared_index];
 	}
 }
+template <class T, std::size_t FRAGMENT_DIM_M = 32, std::size_t FRAGMENT_DIM_N = 16>
+__device__ inline void g2s32x16(
+		T* const shared_ptr, const std::size_t shared_m, const std::size_t shared_n,
+		const T* const global_ptr, const std::size_t global_p_y, const std::size_t global_ld,
+		const unsigned tid
+		){
+	const auto unique_id = tid & 0x3f;
+	const auto y = unique_id & 0x1f;
+	const auto lane = unique_id >> 5;
+	if(y >= shared_m) return;
+	for(std::size_t i = 0; i < FRAGMENT_DIM_N; i+=2){
+		const auto x = i + lane;
+		if(x >= shared_n) return;
+		const auto shared_index = FRAGMENT_DIM_M * x + y;
+		const auto global_index = global_ld * x + y + global_p_y;
+
+		shared_ptr[shared_index] = global_ptr[global_index];
+	}
+}
+template <class T, std::size_t FRAGMENT_DIM_M = 32, std::size_t FRAGMENT_DIM_N = 16>
+__device__ inline void s2g32x16(
+		T* const global_ptr, const std::size_t global_p_y, const std::size_t global_ld,
+		const T* const shared_ptr, const std::size_t shared_m, const std::size_t shared_n,
+		const unsigned tid
+		){
+	const auto unique_id = tid & 0x3f;
+	const auto y = unique_id & 0x1f;
+	const auto lane = unique_id >> 5;
+	if(y >= shared_m) return;
+	for(std::size_t i = 0; i < FRAGMENT_DIM_N; i+=2){
+		const auto x = i + lane;
+		if(x >= shared_n) return;
+		const auto shared_index = FRAGMENT_DIM_M * x + y;
+		const auto global_index = global_ld * x + y + global_p_y;
+
+		global_ptr[global_index] = shared_ptr[shared_index];
+	}
+}
 } // namespace matrix_copy
 } // namespace mtk
 
