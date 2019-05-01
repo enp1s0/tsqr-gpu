c903f8286fcaa25689e00fa41128fd272fc7e2b0
mutsuki <mutsuki@momo86.net>
2019-05-01 14:24:52 +0900

    update : fix argument (unique_id -> tid)

--- src/matrix_operations.cuh
+++ src/matrix_operations.cuh
@@ -6,8 +6,9 @@ namespace matrix_operation {
 template <class T, std::size_t FRAGMENT_DIM_M = 32>
 __device__ inline void make_zero_matrix(
 		T* const target_ptr,
-		const unsigned unique_id
+		const unsigned tid
 		){
+	const auto unique_id = tid & 0x3f;
 	constexpr auto stride = 2 * warp_size;
 	for(unsigned i = 0; i < (FRAGMENT_DIM_M * FRAGMENT_DIM_M) / stride; i++){
 		target_ptr[i * stride + unique_id] = cutf::cuda::type::cast<T>(0.0f);
@@ -17,8 +18,9 @@ __device__ inline void make_zero_matrix(
 template <class T, std::size_t FRAGMENT_DIM_M = 32>
 __device__ inline void make_identity_matrix(
 		T* const target_ptr,
-		const unsigned unique_id
+		const unsigned tid
 		){
+	const auto unique_id = tid & 0x3f;
 	make_zero_matrix(target_ptr, unique_id);
 	target_ptr[unique_id * (1 + FRAGMENT_DIM_M)] = cutf::cuda::type::cast<T>(1.0f);
 }
