36befd09c097424a238b57a54320f41ce910a39c
mutsuki <mutsuki@momo86.net>
2019-05-01 20:57:09 +0900

    update : rm template parameters of qr32x16_f32tc_batched

--- src/tcqr.hpp
+++ src/tcqr.hpp
@@ -4,7 +4,6 @@
 namespace mtk {
 namespace tcqr{
 void qr32x16_f32tc(float* const q, float* const r, const float* const a, const unsigned m, const unsigned n);
-template <class T, class Norm_t, bool UseTC>
 void qr32x16_f32tc_batched(float *const q, float *const r, const float *const a, const unsigned int m, const unsigned int n, const std::size_t batch_size, const unsigned* a_start_position);
 }
 } // namespace mtk
