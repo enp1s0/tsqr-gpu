7798bfff58bdcb495e0bc65dcee43122cb0f2c5c
mutsuki <mutsuki@momo86.net>
2019-05-31 22:31:50 +0900

    Add `qr32x16_batched_kernel`

--- src/tcqr.hpp
+++ src/tcqr.hpp
@@ -5,7 +5,8 @@ namespace mtk {
 namespace tcqr{
 template <class T, bool UseTC>
 void qr32x16(T* const q, T* const r,const T* const a, const unsigned int m, const unsigned int n);
-void qr32x16_f32tc_batched(float *const q, float *const r, const float *const a, const unsigned int m, const unsigned int n, const std::size_t batch_size, const unsigned* a_start_position);
+template <class T, bool UseTC>
+void qr32x16_batched(T *const q, T *const r, const T *const a, const unsigned int m, const unsigned int n, const std::size_t batch_size, const unsigned* a_start_position);
 }
 } // namespace mtk
 
