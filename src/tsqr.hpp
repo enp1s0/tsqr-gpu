a4cd14acf013cc164b7b76f9415252492c040d0d
mutsuki <mutsuki@momo86.net>
2019-06-08 16:38:47 +0900

    Fix template parameters

--- src/tsqr.hpp
+++ src/tsqr.hpp
@@ -3,9 +3,10 @@
 #include <cstddef>
 namespace mtk {
 namespace tsqr {
+// get working memory type
 template <class T, bool UseTC>
-void tsqr16(T* const q_ptr, T* const r_ptr, const T* const a_ptr, const std::size_t m, const std::size_t n, T* const working_memory_ptr);
-std::size_t get_working_memory_size(const std::size_t m, const std::size_t n);
+template <bool UseTC, class T>
+void tsqr16(T* const q_ptr, T* const r_ptr, const T* const a_ptr, const std::size_t m, const std::size_t n, typename get_working_q_type<T, UseTC>::type* const working_q_ptr, typename get_working_r_type<T, UseTC>::type* const working_r_ptr);
 } // namespace tsqr
 } // namespace mtk
 
