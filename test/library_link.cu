ef1fbec8e061288be28c352f5a8fe265e2ae1373
mutsuki <mutsuki@momo86.net>
2019-06-14 00:25:43 +0900

    Fix library_link.cu

--- test/library_link.cu
+++ test/library_link.cu
@@ -3,32 +3,34 @@
 #include <iostream>
 #include <random>
 
-constexpr std::size_t m = 1 << 24;
+constexpr std::size_t m = 1 << 14;
 constexpr std::size_t n = 1 << 4;
 
 int main(){
 	std::mt19937 mt(std::random_device{}());
 	std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
 
-	auto d_a = cutf::cuda::memory::get_device_unique_ptr<float>(m * n);
-	auto d_q = cutf::cuda::memory::get_device_unique_ptr<float>(m * n);
-	auto d_r = cutf::cuda::memory::get_device_unique_ptr<float>(n * n);
-	auto h_a = cutf::cuda::memory::get_host_unique_ptr<float>(m * n);
-	auto h_q = cutf::cuda::memory::get_host_unique_ptr<float>(m * n);
-	auto h_r = cutf::cuda::memory::get_host_unique_ptr<float>(n * n);
+	auto d_a = cutf::memory::get_device_unique_ptr<float>(m * n);
+	auto d_q = cutf::memory::get_device_unique_ptr<float>(m * n);
+	auto d_r = cutf::memory::get_device_unique_ptr<float>(n * n);
+	auto h_a = cutf::memory::get_host_unique_ptr<float>(m * n);
+	auto h_q = cutf::memory::get_host_unique_ptr<float>(m * n);
+	auto h_r = cutf::memory::get_host_unique_ptr<float>(n * n);
 
 	for(std::size_t i = 0; i < m * n; i++){
 		h_a.get()[i] = dist(mt);
 	}
 
-	cutf::cuda::memory::copy(d_a.get(), h_a.get(), m * n);
+	cutf::memory::copy(d_a.get(), h_a.get(), m * n);
 
-	const auto working_memory_size = mtk::tsqr::get_working_memory_size(m, n);
-	auto d_w = cutf::cuda::memory::get_device_unique_ptr<float>(working_memory_size);
+	const auto working_memory_q_size = mtk::tsqr::get_working_q_size(m, n);
+	auto d_wq = cutf::memory::get_device_unique_ptr<typename mtk::tsqr::get_working_q_type<float, true>::type>(working_memory_q_size);
+	const auto working_memory_r_size = mtk::tsqr::get_working_r_size(m, n);
+	auto d_wr = cutf::memory::get_device_unique_ptr<typename mtk::tsqr::get_working_r_type<float, true>::type>(working_memory_r_size);
 
-	mtk::tsqr::tsqr16(
+	mtk::tsqr::tsqr16<true>(
 			d_q.get(), d_r.get(),
 			d_a.get(), m, n, 
-			d_w.get()
+			d_wq.get(), d_wr.get()
 			);
 }
