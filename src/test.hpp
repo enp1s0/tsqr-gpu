fcaf45c9ecc4a73affffd836816e7563ea19e775
mutsuki <mutsuki@momo86.net>
2019-09-28 16:40:48 +0900

    Add test functions for double

--- src/test.hpp
+++ src/test.hpp
@@ -7,7 +7,9 @@ template <bool UseTC, class T>
 void precision(const std::size_t min_m, const std::size_t max_m, const std::size_t n);
 template <bool UseTC, class T>
 void speed(const std::size_t min_m, const std::size_t max_m, const std::size_t n);
+template <class T>
 void cusolver_precision(const std::size_t min_m, const std::size_t max_m, const std::size_t n);
+template <class T>
 void cusolver_speed(const std::size_t min_m, const std::size_t max_m, const std::size_t n);
 } // namespace test
 } // namespace mtk
