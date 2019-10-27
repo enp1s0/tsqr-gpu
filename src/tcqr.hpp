56e8ba2d445bb19841725eca55b4df66d8303474
mutsuki <mutsuki@momo86.net>
2019-10-27 17:00:45 +0900

    Add `Refine` switch to batched qr function

--- src/tcqr.hpp
+++ src/tcqr.hpp
@@ -3,9 +3,9 @@
 
 namespace mtk {
 namespace tcqr{
-template <bool UseTC, class Q_T, class R_T, class A_T>
+template <bool UseTC, bool Refine, class Q_T, class R_T, class A_T>
 void qr32x16(Q_T* const q, R_T* const r,const A_T* const a, const unsigned int m, const unsigned int n);
-template <bool UseTC, class Q_T, class R_T, class A_T>
+template <bool UseTC, bool Refine, class Q_T, class R_T, class A_T>
 void qr32x16_batched(Q_T *const q, R_T *const r, const A_T *const a, const unsigned int m, const unsigned int n, const std::size_t batch_size, const unsigned* a_start_position);
 }
 } // namespace mtk
