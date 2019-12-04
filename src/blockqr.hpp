a93f5c60a3adc3bf3103df7aa061b354bb9a6bde
mutsuki <mutsuki@momo86.net>
2019-12-04 15:58:12 +0900

    Add cublas handle

--- src/blockqr.hpp
+++ src/blockqr.hpp
@@ -1,5 +1,6 @@
 #ifndef __BLOCKQR_HPP__
 #define __BLOCKQR_HPP__
+#include <cutf/cublas.hpp>
 #include <cstddef>
 
 namespace mtk {
@@ -8,7 +9,8 @@ void qr(
 		T* const q_ptr, const std::size_t ldq,
 		T* const r_ptr, const std::size_t ldr,
 		const std::size_t m, const std::size_t n,
-		const T* a_ptr, const std::size_t lda);
+		const T* a_ptr, const std::size_t lda,
+		cublasHandle_t const main_cublas_handle, cublasHandle_t const sub_cublas_handle);
 } // namespace mtk
 
 #endif /* end of include guard */
