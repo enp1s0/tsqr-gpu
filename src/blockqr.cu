ab909c1b7bbf0209ceb67d8dcefce4638edbf1bf
mutsuki <mutsuki@momo86.net>
2019-12-08 15:30:01 +0900

    Add breakdown

--- src/blockqr.cu
+++ src/blockqr.cu
@@ -2,6 +2,18 @@
 #include <cutf/type.hpp>
 #include "blockqr.hpp"
 
+// #define PROFILE_BREAKDOWN
+
+#ifdef PROFILE_BREAKDOWN
+#include <chrono>
+
+namespace  {
+template <class T> std::string get_type_name();
+template <> std::string get_type_name<float>() {return "float";}
+template <> std::string get_type_name<half>() {return "half";}
+} // namespace
+#endif
+
 constexpr std::size_t tsqr_colmun_size = 16;
 
 std::size_t mtk::qr::get_working_q_size(const std::size_t m) {
@@ -30,6 +42,12 @@ void mtk::qr::qr(
 		CUTF_HANDLE_ERROR(cublasSetMathMode(cublas_handle, CUBLAS_TENSOR_OP_MATH));
 	}
 
+#ifdef PROFILE_BREAKDOWN
+	std::size_t gemm_0_count = 0lu;
+	std::size_t gemm_1_count = 0lu;
+	std::size_t tsqr_count = 0lu;
+#endif
+
 	// QR factorization of each block
 	for (std::size_t b = 0; b < column_block_size; b++) {
 		CUTF_HANDLE_ERROR(cudaStreamSynchronize(cuda_stream));
@@ -39,7 +57,13 @@ void mtk::qr::qr(
 		const auto one = cutf::type::cast<T>(1.0f);
 		const auto zero = cutf::type::cast<T>(0.0f);
 		const auto minus_one = cutf::type::cast<T>(-1.0f);
+#ifdef PROFILE_BREAKDOWN
+		std::chrono::time_point<std::chrono::system_clock> t0, t1, t2, t3, t4;
+#endif
 		if (b != 0) {
+#ifdef PROFILE_BREAKDOWN
+			t0 = std::chrono::system_clock::now();
+#endif
 			CUTF_HANDLE_ERROR(cutf::cublas::gemm(
 						cublas_handle,
 						CUBLAS_OP_T, CUBLAS_OP_N,
@@ -50,6 +74,10 @@ void mtk::qr::qr(
 						&zero,
 						r_ptr + ldr * previous_block_n, ldr
 						));
+#ifdef PROFILE_BREAKDOWN
+			CUTF_HANDLE_ERROR(cudaStreamSynchronize(cuda_stream));
+			t1 = std::chrono::system_clock::now();
+#endif
 			// compute A'
 			CUTF_HANDLE_ERROR(cutf::cublas::gemm(
 						cublas_handle,
@@ -61,9 +89,18 @@ void mtk::qr::qr(
 						&one,
 						a_ptr + lda * previous_block_n, lda
 						));
+#ifdef PROFILE_BREAKDOWN
+			CUTF_HANDLE_ERROR(cudaStreamSynchronize(cuda_stream));
+			t2 = std::chrono::system_clock::now();
+#endif
 		}
 		CUTF_HANDLE_ERROR(cudaStreamSynchronize(cuda_stream));
+
 		//QR factorization of A'
+#ifdef PROFILE_BREAKDOWN
+		CUTF_HANDLE_ERROR(cudaStreamSynchronize(cuda_stream));
+		t3 = std::chrono::system_clock::now();
+#endif
 		mtk::tsqr::tsqr16<UseTC, Refinement>(
 				q_ptr + previous_block_n * ldq, ldq,
 				r_ptr + previous_block_n * ldr + previous_block_n, ldr,
@@ -74,7 +111,27 @@ void mtk::qr::qr(
 				cuda_stream
 				);
 		CUTF_HANDLE_ERROR(cudaStreamSynchronize(cuda_stream));
+#ifdef PROFILE_BREAKDOWN
+		t4 = std::chrono::system_clock::now();
+
+		if (b != 0) {
+			gemm_0_count += std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
+			gemm_1_count += std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+		}
+		tsqr_count += std::chrono::duration_cast<std::chrono::microseconds>(t4 - t3).count();
+#endif
 	}
+#ifdef PROFILE_BREAKDOWN
+	const auto time_sum = gemm_0_count + gemm_1_count + tsqr_count;
+	std::printf("# BlockQR breakdown\n");
+	std::printf("Size   : %lu x %lu\n", m, n);
+	std::printf("Type   : %s\n", get_type_name<T>().c_str());
+	std::printf("UseTC  : %s\n", (UseTC ? "YES" : "NO"));
+	std::printf("Refine : %s\n", (Refinement ? "YES" : "NO"));
+	std::printf("GEMM-0 : %e[s] (%e%%)\n", gemm_0_count / 1.0e6, static_cast<double>(gemm_0_count) / time_sum * 100);
+	std::printf("GEMM-1 : %e[s] (%e%%)\n", gemm_1_count / 1.0e6, static_cast<double>(gemm_1_count) / time_sum * 100);
+	std::printf("TSQR   : %e[s] (%e%%)\n", tsqr_count / 1.0e6, static_cast<double>(tsqr_count) / time_sum * 100);
+#endif
 }
 
 template void mtk::qr::qr<false, false, float>(float* const, const std::size_t, float* const, const std::size_t, float* const, const std::size_t, const std::size_t, const std::size_t, typename mtk::qr::get_working_q_type<float, false, false>::type* const, typename mtk::qr::get_working_r_type<float, false, false>::type* const, cublasHandle_t const);
