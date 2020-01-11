ae6f8372a3007123b70c1403183f1c29eac9d9f6
mutsuki <mutsuki@momo86.net>
2020-01-11 15:54:08 +0900

    Rename test.cu and add `test_blockqr` namespace

--- src/test.hpp
+++ src/test.hpp
@@ -2,7 +2,7 @@
 #define __TEST_HPP__
 #include <cstddef>
 #include <type_traits>
-namespace mtk {
+namespace test_tsqr {
 namespace test {
 template <bool UseTC, bool Refine, class T, class CORE_T = T>
 void precision(const std::size_t min_m, const std::size_t max_m, const std::size_t n);
@@ -12,7 +12,18 @@ template <class T>
 void cusolver_precision(const std::size_t min_m, const std::size_t max_m, const std::size_t n);
 template <class T>
 void cusolver_speed(const std::size_t min_m, const std::size_t max_m, const std::size_t n);
-} // namespace test
+} // namespace test_tsqr
+
+namespace test_blockqr {
+template <bool UseTC, bool Refine, class T, class CORE_T = T>
+void precision(const std::size_t min_m, const std::size_t max_m);
+template <bool UseTC, bool Refine, class T, class CORE_T = T>
+void speed(const std::size_t min_m, const std::size_t max_m);
+template <class T>
+void cusolver_precision(const std::size_t min_m, const std::size_t max_m);
+template <class T>
+void cusolver_speed(const std::size_t min_m, const std::size_t max_m);
+} // namespace test_blockqr
 } // namespace mtk
 
 #endif /* end of include guard */
