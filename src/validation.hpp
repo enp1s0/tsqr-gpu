69087c09c944316d6a3c2b1e26fcb94ed86b7ae7
mutsuki <mutsuki@momo86.net>
2020-01-12 13:51:06 +0900

    Add declaration of `check_submatrix_orthogonality`

--- src/validation.hpp
+++ src/validation.hpp
@@ -6,6 +6,8 @@ namespace mtk {
 namespace validation {
 template <class T>
 double check_orthogonality16(const T* const matrix, const std::size_t m, const unsigned n);
+template <class T>
+void check_submatrix_orthogonality(const T* const matrix, const std::size_t m, const unsigned n);
 } // namespace validation
 } // namespace mtk
 
