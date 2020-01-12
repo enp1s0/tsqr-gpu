92134cbc129c7f29081dc81d26212d3432bf9b6e
mutsuki <mutsuki@momo86.net>
2020-01-12 18:24:49 +0900

    Update argument of test functions

--- src/test.hpp
+++ src/test.hpp
@@ -2,28 +2,19 @@
 #define __TEST_HPP__
 #include <cstddef>
 #include <type_traits>
+#include <utility>
+#include <vector>
 namespace mtk {
-namespace test_tsqr {
+namespace test_qr {
 template <bool UseTC, bool Refine, class T, class CORE_T = T>
-void precision(const std::size_t min_m, const std::size_t max_m, const std::size_t n);
+void precision(const std::vector<std::pair<std::size_t, std::size_t>>& size_pair_vector);
 template <bool UseTC, bool Refine, class T, class CORE_T = T>
-void speed(const std::size_t min_m, const std::size_t max_m, const std::size_t n);
+void speed(const std::vector<std::pair<std::size_t, std::size_t>>& size_pair_vector);
 template <class T>
-void cusolver_precision(const std::size_t min_m, const std::size_t max_m, const std::size_t n);
+void cusolver_precision(const std::vector<std::pair<std::size_t, std::size_t>>& size_pair_vector);
 template <class T>
-void cusolver_speed(const std::size_t min_m, const std::size_t max_m, const std::size_t n);
-} // namespace test_tsqr
-
-namespace test_blockqr {
-template <bool UseTC, bool Refine, class T, class CORE_T = T>
-void precision(const std::size_t min_m, const std::size_t max_m);
-template <bool UseTC, bool Refine, class T, class CORE_T = T>
-void speed(const std::size_t min_m, const std::size_t max_m);
-template <class T>
-void cusolver_precision(const std::size_t min_m, const std::size_t max_m);
-template <class T>
-void cusolver_speed(const std::size_t min_m, const std::size_t max_m);
-} // namespace test_blockqr
+void cusolver_speed(const std::vector<std::pair<std::size_t, std::size_t>>& size_pair_vector);
+} // namespace test_qr
 } // namespace mtk
 
 #endif /* end of include guard */
