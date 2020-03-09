d86fa4dc2950c9f60c3564e6439170f1b2653070
mutsuki <mutsuki@momo86.net>
2020-03-09 15:39:55 +0900

    Add `buffer` struct

--- src/tsqr.hpp
+++ src/tsqr.hpp
@@ -23,6 +23,35 @@ inline std::size_t get_working_l_size(const std::size_t m) {
 	return get_batch_size(m) + 1lu;
 }
 
+// integrated buffer struct
+template <class T, bool UseTC, bool Refine>
+struct buffer {
+	typename get_working_q_type<T, UseTC, Refine>::type* dwq;
+	typename get_working_r_type<T, UseTC, Refine>::type* dwr;
+	unsigned* dl;
+	unsigned* hl;
+
+	// constructor
+	buffer() : dwq(nullptr), dwr(nullptr), dl(nullptr), hl(nullptr) {}
+	// destructor
+	~buffer() {
+		destroy();
+	}
+
+	void allocate(const std::size_t m, const std::size_t n) {
+		cudaMalloc(reinterpret_cast<void**>(&dwq), sizeof(typename get_working_q_type<T, UseTC, Refine>::type) * get_working_q_size(m, n));
+		cudaMalloc(reinterpret_cast<void**>(&dwr), sizeof(typename get_working_r_type<T, UseTC, Refine>::type) * get_working_r_size(m, n));
+		cudaMalloc(reinterpret_cast<void**>(&dl), sizeof(unsigned) * get_working_l_size(m));
+		cudaMallocHost(reinterpret_cast<void**>(&dl), sizeof(unsigned) * get_working_l_size(m));
+	}
+	void destroy() {
+		cudaFree(dwq); dwq = nullptr;
+		cudaFree(dwr); dwr = nullptr;
+		cudaFree(dl); dl = nullptr;
+		cudaFreeHost(hl); hl = nullptr;
+	}
+};
+
 template <bool UseTC, bool Refine, class T, class CORE_T>
 void tsqr16(
 		T* const q_ptr, const std::size_t ldq,
