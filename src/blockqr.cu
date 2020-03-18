2ffcc7af4464d0776e4381fa9381a23c1cea0c8d
mutsuki <mutsuki@momo86.net>
2020-03-18 20:32:02 +0900

    Add `block_qr_reorthogonalization_core`

--- src/blockqr.cu
+++ src/blockqr.cu
@@ -163,6 +163,157 @@ mtk::qr::state_t block_qr_core(
 	return mtk::qr::success_factorization;
 }
 
+template <bool UseTC, bool Refinement, class T, class CORE_T>
+mtk::qr::state_t block_qr_reorthogonalization_core(
+		T* const q_ptr, const std::size_t ldq,
+		T* const r_ptr, const std::size_t ldr,
+		T* const a_ptr, const std::size_t lda,
+		const std::size_t m, const std::size_t n,
+		typename mtk::qr::get_working_q_type<T, UseTC, Refinement>::type* const wq_ptr,
+		typename mtk::qr::get_working_r_type<T, UseTC, Refinement>::type* const wr_ptr,
+		T* const w_reorth,
+		unsigned* const d_wl_ptr,
+		unsigned* const h_wl_ptr,
+		cublasHandle_t const cublas_handle) {
+	const auto column_block_size = (n + mtk::qr::tsqr_colmun_size - 1) / mtk::qr::tsqr_colmun_size;
+
+	T* const a2_ptr = w_reorth;
+	T* const r2_ptr = a2_ptr + m * mtk::qr::tsqr_colmun_size;
+	T* const r12_2_ptr = r2_ptr + mtk::qr::tsqr_colmun_size * mtk::qr::tsqr_colmun_size;
+	T* const r3_ptr = r12_2_ptr + mtk::qr::tsqr_colmun_size * mtk::qr::tsqr_colmun_size;
+
+	cudaStream_t cuda_stream;
+	CUTF_HANDLE_ERROR(cublasGetStream(cublas_handle, &cuda_stream));
+
+	cublasMath_t original_math_mode;
+	CUTF_HANDLE_ERROR(cublasGetMathMode(cublas_handle, &original_math_mode));
+
+	if (UseTC && !Refinement) {
+		CUTF_HANDLE_ERROR(cublasSetMathMode(cublas_handle, CUBLAS_TENSOR_OP_MATH));
+	} else {
+		CUTF_HANDLE_ERROR(cublasSetMathMode(cublas_handle, CUBLAS_DEFAULT_MATH));
+	}
+
+#ifdef PROFILE_BREAKDOWN
+	std::size_t gemm_0_count = 0lu;
+	std::size_t gemm_1_count = 0lu;
+	std::size_t tsqr_count = 0lu;
+#endif
+
+	// QR factorization of each block
+	for (std::size_t b = 0; b < column_block_size; b++) {
+		CUTF_HANDLE_ERROR(cudaStreamSynchronize(cuda_stream));
+
+		const auto current_block_n = std::min(mtk::qr::tsqr_colmun_size, n - b * mtk::qr::tsqr_colmun_size);
+		const auto previous_block_n = b * mtk::qr::tsqr_colmun_size;
+		const auto one = cutf::type::cast<T>(1.0f);
+		const auto zero = cutf::type::cast<T>(0.0f);
+		const auto minus_one = cutf::type::cast<T>(-1.0f);
+
+		if (b != 0) {
+			CUTF_HANDLE_ERROR(cutf::cublas::gemm(
+						cublas_handle,
+						CUBLAS_OP_T, CUBLAS_OP_N,
+						previous_block_n, current_block_n, m,
+						&one,
+						q_ptr, ldq,
+						a_ptr + lda * previous_block_n, lda,
+						&zero,
+						r_ptr + ldr * previous_block_n, ldr
+						));
+			// compute A'
+			CUTF_HANDLE_ERROR(cutf::cublas::gemm(
+						cublas_handle,
+						CUBLAS_OP_N, CUBLAS_OP_N,
+						m, current_block_n, previous_block_n,
+						&minus_one,
+						q_ptr, ldq,
+						r_ptr + ldr * previous_block_n, ldr,
+						&one,
+						a2_ptr, lda
+						));
+			mtk::tsqr::tsqr16<UseTC, Refinement, T, CORE_T>(
+					q_ptr + previous_block_n * ldq, ldq,
+					r2_ptr, current_block_n,
+					a2_ptr, lda,
+					m, current_block_n,
+					wq_ptr,
+					wr_ptr,
+					d_wl_ptr,
+					h_wl_ptr,
+					cuda_stream
+					);
+			CUTF_HANDLE_ERROR(cutf::cublas::gemm(
+						cublas_handle,
+						CUBLAS_OP_T, CUBLAS_OP_N,
+						current_block_n, current_block_n, m,
+						&one,
+						q_ptr + previous_block_n * ldq, ldq,
+						a2_ptr, lda,
+						&zero,
+						r12_2_ptr, current_block_n
+						));
+			CUTF_HANDLE_ERROR(cutf::cublas::gemm(
+						cublas_handle,
+						CUBLAS_OP_N, CUBLAS_OP_N,
+						m, current_block_n, previous_block_n,
+						&minus_one,
+						q_ptr + previous_block_n * ldq, ldq,
+						r12_2_ptr, current_block_n,
+						&one,
+						a2_ptr, lda
+						));
+			mtk::tsqr::tsqr16<UseTC, Refinement, T, CORE_T>(
+					q_ptr + previous_block_n * ldq, ldq,
+					r3_ptr, current_block_n,
+					a2_ptr, lda,
+					m, current_block_n,
+					wq_ptr,
+					wr_ptr,
+					d_wl_ptr,
+					h_wl_ptr,
+					cuda_stream
+					);
+			CUTF_HANDLE_ERROR(cutf::cublas::gemm(
+						cublas_handle,
+						CUBLAS_OP_N, CUBLAS_OP_N,
+						previous_block_n, current_block_n, current_block_n,
+						&one,
+						r12_2_ptr, current_block_n,
+						r2_ptr, current_block_n,
+						&one,
+						r_ptr + ldr * previous_block_n, ldr
+						));
+			CUTF_HANDLE_ERROR(cutf::cublas::gemm(
+						cublas_handle,
+						CUBLAS_OP_N, CUBLAS_OP_N,
+						current_block_n, current_block_n, current_block_n,
+						&one,
+						r3_ptr, current_block_n,
+						r2_ptr, current_block_n,
+						&zero,
+						r_ptr + previous_block_n * ldr + previous_block_n, ldr
+						));
+		} else {
+			mtk::tsqr::tsqr16<UseTC, Refinement, T, CORE_T>(
+					q_ptr + previous_block_n * ldq, ldq,
+					r_ptr + previous_block_n * ldr + previous_block_n, ldr,
+					a_ptr + previous_block_n * lda, lda,
+					m, current_block_n,
+					wq_ptr,
+					wr_ptr,
+					d_wl_ptr,
+					h_wl_ptr,
+					cuda_stream
+					);
+		}
+		CUTF_HANDLE_ERROR(cudaStreamSynchronize(cuda_stream));
+	}
+	CUTF_HANDLE_ERROR(cublasSetMathMode(cublas_handle, original_math_mode));
+
+	return mtk::qr::success_factorization;
+}
+
 } // namespace
 
 template <bool UseTC, bool Refinement, bool Reorthoganalize, class T, class CORE_T>
