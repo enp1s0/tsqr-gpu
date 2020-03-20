82bed1510e762f681061ec8bd65db05b45d428a8
mutsuki <mutsuki@momo86.net>
2020-03-20 15:55:07 +0900

    Add the test with reorthogonalization

--- src/main.cu
+++ src/main.cu
@@ -5,21 +5,33 @@
 
 void qr_test(const std::vector<std::pair<std::size_t, std::size_t>>& test_matrix_size_array) {
 	std::cout << "# precision test" << std::endl;
-	mtk::test_qr::precision<true, false, false, float>(test_matrix_size_array);
-	mtk::test_qr::precision<true, false, false, half>(test_matrix_size_array);
-	mtk::test_qr::precision<true, false, false, float, half>(test_matrix_size_array);
+	mtk::test_qr::precision<true , false, false, float>(test_matrix_size_array);
+	mtk::test_qr::precision<true , false, false, half >(test_matrix_size_array);
+	mtk::test_qr::precision<true , false, false, float, half>(test_matrix_size_array);
 	mtk::test_qr::precision<false, false, false, float>(test_matrix_size_array);
-	mtk::test_qr::precision<false, false, false, half>(test_matrix_size_array);
-	mtk::test_qr::precision<true, true, false, float>(test_matrix_size_array);
+	mtk::test_qr::precision<false, false, false, half >(test_matrix_size_array);
+	mtk::test_qr::precision<true , true , false, float>(test_matrix_size_array);
+	mtk::test_qr::precision<true , false, true , float>(test_matrix_size_array);
+	mtk::test_qr::precision<true , false, true , half >(test_matrix_size_array);
+	mtk::test_qr::precision<true , false, true , float, half>(test_matrix_size_array);
+	mtk::test_qr::precision<false, false, true , float>(test_matrix_size_array);
+	mtk::test_qr::precision<false, false, true , half >(test_matrix_size_array);
+	mtk::test_qr::precision<true , true , true , float>(test_matrix_size_array);
 	mtk::test_qr::cusolver_precision<float>(test_matrix_size_array);
 	mtk::test_qr::cusolver_precision<double>(test_matrix_size_array);
 	std::cout << "# speed test" << std::endl;
-	mtk::test_qr::speed<true, false, false, float>(test_matrix_size_array);
-	mtk::test_qr::speed<true, false, false, half>(test_matrix_size_array);
-	mtk::test_qr::speed<true, false, false, float, half>(test_matrix_size_array);
+	mtk::test_qr::speed<true , false, false, float>(test_matrix_size_array);
+	mtk::test_qr::speed<true , false, false, half >(test_matrix_size_array);
+	mtk::test_qr::speed<true , false, false, float, half>(test_matrix_size_array);
 	mtk::test_qr::speed<false, false, false, float>(test_matrix_size_array);
-	mtk::test_qr::speed<false, false, false, half>(test_matrix_size_array);
-	mtk::test_qr::speed<true, true, false, float>(test_matrix_size_array);
+	mtk::test_qr::speed<false, false, false, half >(test_matrix_size_array);
+	mtk::test_qr::speed<true , true , false, float>(test_matrix_size_array);
+	mtk::test_qr::speed<true , false, true , float>(test_matrix_size_array);
+	mtk::test_qr::speed<true , false, true , half >(test_matrix_size_array);
+	mtk::test_qr::speed<true , false, true , float, half>(test_matrix_size_array);
+	mtk::test_qr::speed<false, false, true , float>(test_matrix_size_array);
+	mtk::test_qr::speed<false, false, true , half >(test_matrix_size_array);
+	mtk::test_qr::speed<true , true , true , float>(test_matrix_size_array);
 	mtk::test_qr::cusolver_speed<float>(test_matrix_size_array);
 	mtk::test_qr::cusolver_speed<double>(test_matrix_size_array);
 }
