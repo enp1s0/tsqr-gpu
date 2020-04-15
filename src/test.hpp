28aae5a910b4afe49d3544a252277689be2a7599
mutsuki <mutsuki@momo86.net>
2020-04-15 15:22:14 +0900

    Add condition number test

--- src/test.hpp
+++ src/test.hpp
@@ -4,6 +4,7 @@
 #include <type_traits>
 #include <utility>
 #include <vector>
+#include <tuple>
 namespace mtk {
 namespace test_qr {
 template <bool UseTC, bool Refine, bool Reorthogonalize, class T, class CORE_T = T>
@@ -14,6 +15,11 @@ template <class T>
 void cusolver_precision(const std::vector<std::pair<std::size_t, std::size_t>>& size_pair_vector, const std::size_t C = 16);
 template <class T>
 void cusolver_speed(const std::vector<std::pair<std::size_t, std::size_t>>& size_pair_vector, const std::size_t C = 16);
+
+template <bool UseTC, bool Refine, bool Reorthogonalize, class T, class CORE_T = T>
+void precision_cond(const std::vector<std::tuple<std::size_t, std::size_t, float>>& size_pair_vector, const std::size_t C = 16);
+template <class T>
+void cusolver_precision_cond(const std::vector<std::tuple<std::size_t, std::size_t, float>>& size_pair_vector, const std::size_t C = 16);
 } // namespace test_qr
 } // namespace mtk
 
