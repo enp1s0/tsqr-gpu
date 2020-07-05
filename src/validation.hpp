a675b16c3dffb75a399a23a92825350f26a1a851
mutsuki <mutsuki@momo86.net>
2020-07-05 10:21:24 +0900

    Add the declaration of `multi_orthogonality`

--- src/validation.hpp
+++ src/validation.hpp
@@ -8,6 +8,8 @@ template <class T>
 double check_orthogonality16(const T* const matrix, const std::size_t m, const unsigned n);
 template <class T>
 void check_submatrix_orthogonality(const T* const matrix, const std::size_t m, const unsigned n);
+template <class T>
+void multi_orthogonality(const T* const ptr, const std::size_t m, const std::size_t n, const std::size_t size);
 } // namespace validation
 } // namespace mtk
 
