fe52c325495a2965788980623e40d0d53870ebce
mutsuki <mutsuki@momo86.net>
2020-07-21 14:16:12 +0900

    Update mtk::matmul::gemm/matmul_core16x16

--- src/a100_tc_emulator.hpp
+++ src/a100_tc_emulator.hpp
@@ -1,49 +1,224 @@
 #ifndef __A100_TC_EMULATOR_HPP__
 #define __A100_TC_EMULATOR_HPP__
 #include <cutf/debug/tf32.hpp>
-
-// If this flag is not defined, error correction does not work.
-#define A100_TC_COR
+#include <cutf/type.hpp>
+#include "gemm_core/gemm_core.cuh"
 
 namespace mtk {
-namespace a100_tc_cor {
-#ifdef A100_TC_COR
-__device__ inline void gemm_core16x16(float* const c, const unsigned ldm_c, const float* const a, const unsigned ldm_a, const float* const b, const unsigned ldm_b, const unsigned unique_id){
+namespace matmul {
+enum compute_mode {
+	fp16_notc,
+	fp32_notc,
+	tf32_tc_cor_emu,
+	tf32_tc_nocor_emu,
+	mixed_tc_cor,
+};
+
+template <mtk::matmul::compute_mode mode, class T>
+__device__ inline void gemm_core16x16(T* const c, const unsigned ldm_c, const T* const a, const unsigned ldm_a, const T* const b, const unsigned ldm_b, const unsigned unique_id) {
+	mtk::gemm_core16x16<T, 1>(c, ldm_c, a, ldm_a, b, ldm_b, unique_id);
+}
+
+template <> __device__ inline void gemm_core16x16<mtk::matmul::compute_mode::tf32_tc_cor_emu, float>(float* const c, const unsigned ldm_c, const float* const a, const unsigned ldm_a, const float* const b, const unsigned ldm_b, const unsigned unique_id) {
+	constexpr unsigned warp_size = 32;
 	const auto lane = unique_id >> 4;
 	const auto y = unique_id & 0xf;
-	for(auto i = 0; i < 16; i += 2){
+	float tmp_a[16];
+	float tmp_c[16 * 16 / warp_size];
+
+	for (auto i = 0; i < 16; i++){
+		tmp_a[i] = a[y + ldm_a * i];
+	}
+
+	for (auto i = 0; i < 16; i += 2){
 		const auto x = i + lane;
 		float sum_ab = 0.0f;
 		float sum_dab = 0.0f;
 		float sum_adb = 0.0f;
 		for(unsigned k = 0; k < 16; k += 1){
-			const auto a_v = cutf::debug::tf32::to_tf32(a[y + ldm_a * k]);
+			const auto a_v = cutf::debug::tf32::to_tf32(tmp_a[k]);
 			const auto b_v = cutf::debug::tf32::to_tf32(b[x * ldm_b + k]);
-			const auto a_dv = cutf::debug::tf32::to_tf32(a[y + ldm_a * k] - a_v);
+			const auto a_dv = cutf::debug::tf32::to_tf32(tmp_a[k] - a_v);
 			const auto b_dv = cutf::debug::tf32::to_tf32(b[x * ldm_b + k] - b_v);
 			sum_ab = fmaf(a_v, b_v, sum_ab);
 			sum_dab = fmaf(a_dv, b_v, sum_dab);
 			sum_adb = fmaf(a_v, b_dv, sum_adb);
 		}
-		c[x * ldm_c + y] += sum_adb + sum_dab + sum_ab;
+		tmp_c[i / 2] += sum_adb + sum_dab + sum_ab;
 	}
-}
-#else
-__device__ inline void gemm_core16x16(float* const c, const unsigned ldm_c, const float* const a, const unsigned ldm_a, const float* const b, const unsigned ldm_b, const unsigned unique_id){
-	const auto lane = unique_id >> 4;
-	const auto y = unique_id & 0xf;
+
 	for(auto i = 0; i < 16; i += 2){
 		const auto x = i + lane;
+		c[x * ldm_c + y] += tmp_c[i / 2];
+	}
+}
+
+template <> __device__ inline void gemm_core16x16<mtk::matmul::compute_mode::mixed_tc_cor, float>(float* const c, const unsigned ldm_c, const float* const a, const unsigned ldm_a, const float* const b, const unsigned ldm_b, const unsigned unique_id) {
+	constexpr unsigned warp_size = 32;
+	const auto lane = unique_id >> 4;
+	const auto y = unique_id & 0xf;
+	float tmp_a[16];
+	float tmp_c[16 * 16 / warp_size];
+
+	for (auto i = 0; i < 16; i++){
+		tmp_a[i] = a[y + ldm_a * i];
+	}
+
+	for (auto i = 0; i < 16; i += 2){
+		const auto x = i + lane;
 		float sum_ab = 0.0f;
+		float sum_dab = 0.0f;
+		float sum_adb = 0.0f;
 		for(unsigned k = 0; k < 16; k += 1){
-			const auto a_v = cutf::debug::tf32::to_tf32(a[y + ldm_a * k]);
+			const auto a_v = cutf::type::cast<float>(cutf::type::cast<half>(tmp_a[k]));
+			const auto b_v = cutf::type::cast<float>(cutf::type::cast<half>(b[x * ldm_b + k]));
+			const auto a_dv = cutf::debug::tf32::to_tf32(tmp_a[k] - a_v);
+			const auto b_dv = cutf::debug::tf32::to_tf32(b[x * ldm_b + k] - b_v);
+			sum_ab = fmaf(a_v, b_v, sum_ab);
+			sum_dab = fmaf(a_dv, b_v, sum_dab);
+			sum_adb = fmaf(a_v, b_dv, sum_adb);
+		}
+		tmp_c[i / 2] += sum_adb + sum_dab + sum_ab;
+	}
+
+	for(auto i = 0; i < 16; i += 2){
+		const auto x = i + lane;
+		c[x * ldm_c + y] += tmp_c[i / 2];
+	}
+}
+
+template <> __device__ inline void gemm_core16x16<mtk::matmul::compute_mode::tf32_tc_nocor_emu, float>(float* const c, const unsigned ldm_c, const float* const a, const unsigned ldm_a, const float* const b, const unsigned ldm_b, const unsigned unique_id) {
+	constexpr unsigned warp_size = 32;
+	const auto lane = unique_id >> 4;
+	const auto y = unique_id & 0xf;
+	float tmp_a[16];
+	float tmp_c[16 * 16 / warp_size];
+
+	for (auto i = 0; i < 16; i++){
+		tmp_a[i] = a[y + ldm_a * i];
+	}
+
+	for (auto i = 0; i < 16; i += 2){
+		const auto x = i + lane;
+		float sum_ab = 0.0f;
+		float sum_dab = 0.0f;
+		float sum_adb = 0.0f;
+		for(unsigned k = 0; k < 16; k += 1){
+			const auto a_v = cutf::debug::tf32::to_tf32(tmp_a[k]);
 			const auto b_v = cutf::debug::tf32::to_tf32(b[x * ldm_b + k]);
 			sum_ab = fmaf(a_v, b_v, sum_ab);
 		}
-		c[x * ldm_c + y] += sum_ab;
+		tmp_c[i / 2] += sum_adb + sum_dab + sum_ab;
+	}
+
+	for(auto i = 0; i < 16; i += 2){
+		const auto x = i + lane;
+		c[x * ldm_c + y] += tmp_c[i / 2];
 	}
 }
-#endif //A100_TC_COR
+
+template <mtk::matmul::compute_mode mode, class T>
+__device__ inline void matmul_core16x16(T* const c, const unsigned ldm_c, const T* const a, const unsigned ldm_a, const T* const b, const unsigned ldm_b, const unsigned unique_id) {
+	mtk::matmul_core16x16<T, 1>(c, ldm_c, a, ldm_a, b, ldm_b, unique_id);
 }
+
+template <> __device__ inline void matmul_core16x16<mtk::matmul::compute_mode::tf32_tc_cor_emu, float>(float* const c, const unsigned ldm_c, const float* const a, const unsigned ldm_a, const float* const b, const unsigned ldm_b, const unsigned unique_id) {
+	constexpr unsigned warp_size = 32;
+	const auto lane = unique_id >> 4;
+	const auto y = unique_id & 0xf;
+	float tmp_a[16];
+	float tmp_c[16 * 16 / warp_size];
+
+	for (auto i = 0; i < 16; i++){
+		tmp_a[i] = a[y + ldm_a * i];
+	}
+
+	for (auto i = 0; i < 16; i += 2){
+		const auto x = i + lane;
+		float sum_ab = 0.0f;
+		float sum_dab = 0.0f;
+		float sum_adb = 0.0f;
+		for(unsigned k = 0; k < 16; k += 1){
+			const auto a_v = cutf::debug::tf32::to_tf32(tmp_a[k]);
+			const auto b_v = cutf::debug::tf32::to_tf32(b[x * ldm_b + k]);
+			const auto a_dv = cutf::debug::tf32::to_tf32(tmp_a[k] - a_v);
+			const auto b_dv = cutf::debug::tf32::to_tf32(b[x * ldm_b + k] - b_v);
+			sum_ab = fmaf(a_v, b_v, sum_ab);
+			sum_dab = fmaf(a_dv, b_v, sum_dab);
+			sum_adb = fmaf(a_v, b_dv, sum_adb);
+		}
+		tmp_c[i / 2] += sum_adb + sum_dab + sum_ab;
+	}
+
+	for(auto i = 0; i < 16; i += 2){
+		const auto x = i + lane;
+		c[x * ldm_c + y] = tmp_c[i / 2];
+	}
 }
+
+template <> __device__ inline void matmul_core16x16<mtk::matmul::compute_mode::mixed_tc_cor, float>(float* const c, const unsigned ldm_c, const float* const a, const unsigned ldm_a, const float* const b, const unsigned ldm_b, const unsigned unique_id) {
+	constexpr unsigned warp_size = 32;
+	const auto lane = unique_id >> 4;
+	const auto y = unique_id & 0xf;
+	float tmp_a[16];
+	float tmp_c[16 * 16 / warp_size];
+
+	for (auto i = 0; i < 16; i++){
+		tmp_a[i] = a[y + ldm_a * i];
+	}
+
+	for (auto i = 0; i < 16; i += 2){
+		const auto x = i + lane;
+		float sum_ab = 0.0f;
+		float sum_dab = 0.0f;
+		float sum_adb = 0.0f;
+		for(unsigned k = 0; k < 16; k += 1){
+			const auto a_v = cutf::type::cast<float>(cutf::type::cast<half>(tmp_a[k]));
+			const auto b_v = cutf::type::cast<float>(cutf::type::cast<half>(b[x * ldm_b + k]));
+			const auto a_dv = cutf::debug::tf32::to_tf32(tmp_a[k] - a_v);
+			const auto b_dv = cutf::debug::tf32::to_tf32(b[x * ldm_b + k] - b_v);
+			sum_ab = fmaf(a_v, b_v, sum_ab);
+			sum_dab = fmaf(a_dv, b_v, sum_dab);
+			sum_adb = fmaf(a_v, b_dv, sum_adb);
+		}
+		tmp_c[i / 2] += sum_adb + sum_dab + sum_ab;
+	}
+
+	for(auto i = 0; i < 16; i += 2){
+		const auto x = i + lane;
+		c[x * ldm_c + y] = tmp_c[i / 2];
+	}
+}
+
+template <> __device__ inline void matmul_core16x16<mtk::matmul::compute_mode::tf32_tc_nocor_emu, float>(float* const c, const unsigned ldm_c, const float* const a, const unsigned ldm_a, const float* const b, const unsigned ldm_b, const unsigned unique_id) {
+	constexpr unsigned warp_size = 32;
+	const auto lane = unique_id >> 4;
+	const auto y = unique_id & 0xf;
+	float tmp_a[16];
+	float tmp_c[16 * 16 / warp_size];
+
+	for (auto i = 0; i < 16; i++){
+		tmp_a[i] = a[y + ldm_a * i];
+	}
+
+	for (auto i = 0; i < 16; i += 2){
+		const auto x = i + lane;
+		float sum_ab = 0.0f;
+		float sum_dab = 0.0f;
+		float sum_adb = 0.0f;
+		for(unsigned k = 0; k < 16; k += 1){
+			const auto a_v = cutf::debug::tf32::to_tf32(tmp_a[k]);
+			const auto b_v = cutf::debug::tf32::to_tf32(b[x * ldm_b + k]);
+			sum_ab = fmaf(a_v, b_v, sum_ab);
+		}
+		tmp_c[i / 2] += sum_adb + sum_dab + sum_ab;
+	}
+
+	for(auto i = 0; i < 16; i += 2){
+		const auto x = i + lane;
+		c[x * ldm_c + y] = tmp_c[i / 2];
+	}
+}
+} // namespace a100_tc_cor
+} // namespace mtk
 #endif
