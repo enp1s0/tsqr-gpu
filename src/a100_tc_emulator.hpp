d8042399c83b86439fcd938c7e184ef47aabe989
mutsuki <mutsuki@momo86.net>
2020-07-26 16:13:42 +0900

    Update `matmul_core_m16n16k32`

--- src/a100_tc_emulator.hpp
+++ src/a100_tc_emulator.hpp
@@ -23,31 +23,27 @@ template <> __device__ inline void matmul_core_m16n16k32<mtk::matmul::compute_mo
 	constexpr unsigned warp_size = 32;
 	const auto lane = unique_id >> 4;
 	const auto y = unique_id & 0xf;
-	float tmp_a[16];
 	float tmp_c[16 * 16 / warp_size];
 
-	for (auto i = 0; i < 16; i++){
-		tmp_a[i] = a[y + ldm_a * i];
-	}
-
-	for (auto i = 0; i < 16; i += 2){
+	for (auto i = 0; i < 16; i += 2) {
 		const auto x = i + lane;
 		float sum_ab = 0.0f;
 		float sum_dab = 0.0f;
 		float sum_adb = 0.0f;
-		for(unsigned k = 0; k < 16; k += 1){
-			const auto a_v = cutf::experimental::tf32::to_tf32(tmp_a[k]);
-			const auto b_v = cutf::experimental::tf32::to_tf32(b[x * ldm_b + k]);
-			const auto a_dv = cutf::experimental::tf32::to_tf32(tmp_a[k] - a_v);
-			const auto b_dv = cutf::experimental::tf32::to_tf32(b[x * ldm_b + k] - b_v);
+		for (unsigned k = 0; k < 32; k += 1) {
+			const auto a_v =  cutf::type::cast<nvcuda::wmma::precision::tf32>(a[y + ldm_a * k]);
+			const auto b_v =  cutf::type::cast<nvcuda::wmma::precision::tf32>(b[x * ldm_b + k]);
+			const auto a_dv = cutf::type::cast<nvcuda::wmma::precision::tf32>(a[y + ldm_a * k] - a_v);
+			const auto b_dv = cutf::type::cast<nvcuda::wmma::precision::tf32>(b[x * ldm_b + k] - b_v);
 			sum_ab = fmaf(a_v, b_v, sum_ab);
 			sum_dab = fmaf(a_dv, b_v, sum_dab);
 			sum_adb = fmaf(a_v, b_dv, sum_adb);
 		}
-		tmp_c[i / 2] += sum_adb + sum_dab + sum_ab;
+		tmp_c[i / 2] = sum_ab + (sum_adb + sum_dab);
 	}
 
-	for(auto i = 0; i < 16; i += 2){
+	__syncthreads();
+	for (auto i = 0; i < 16; i += 2) {
 		const auto x = i + lane;
 		c[x * ldm_c + y] = tmp_c[i / 2];
 	}
@@ -57,31 +53,27 @@ template <> __device__ inline void matmul_core_m16n16k32<mtk::matmul::compute_mo
 	constexpr unsigned warp_size = 32;
 	const auto lane = unique_id >> 4;
 	const auto y = unique_id & 0xf;
-	float tmp_a[16];
 	float tmp_c[16 * 16 / warp_size];
 
-	for (auto i = 0; i < 16; i++){
-		tmp_a[i] = a[y + ldm_a * i];
-	}
-
-	for (auto i = 0; i < 16; i += 2){
+	for (auto i = 0; i < 16; i += 2) {
 		const auto x = i + lane;
 		float sum_ab = 0.0f;
 		float sum_dab = 0.0f;
 		float sum_adb = 0.0f;
-		for(unsigned k = 0; k < 16; k += 1){
-			const auto a_v = cutf::type::cast<float>(cutf::type::cast<half>(tmp_a[k]));
-			const auto b_v = cutf::type::cast<float>(cutf::type::cast<half>(b[x * ldm_b + k]));
-			const auto a_dv = cutf::experimental::tf32::to_tf32(tmp_a[k] - a_v);
-			const auto b_dv = cutf::experimental::tf32::to_tf32(b[x * ldm_b + k] - b_v);
+		for (unsigned k = 0; k < 32; k += 1) {
+			const auto a_v =  cutf::type::cast<half>(a[y + ldm_a * k]);
+			const auto b_v =  cutf::type::cast<half>(b[x * ldm_b + k]);
+			const auto a_dv = cutf::type::cast<nvcuda::wmma::precision::tf32>(a[y + ldm_a * k] - cutf::type::cast<float>(a_v));
+			const auto b_dv = cutf::type::cast<nvcuda::wmma::precision::tf32>(b[x * ldm_b + k] - cutf::type::cast<float>(b_v));
 			sum_ab = fmaf(a_v, b_v, sum_ab);
 			sum_dab = fmaf(a_dv, b_v, sum_dab);
 			sum_adb = fmaf(a_v, b_dv, sum_adb);
 		}
-		tmp_c[i / 2] += sum_adb + sum_dab + sum_ab;
+		tmp_c[i / 2] = sum_ab + (sum_adb + sum_dab);
 	}
 
-	for(auto i = 0; i < 16; i += 2){
+	__syncthreads();
+	for (auto i = 0; i < 16; i += 2) {
 		const auto x = i + lane;
 		c[x * ldm_c + y] = tmp_c[i / 2];
 	}
@@ -91,27 +83,21 @@ template <> __device__ inline void matmul_core_m16n16k32<mtk::matmul::compute_mo
 	constexpr unsigned warp_size = 32;
 	const auto lane = unique_id >> 4;
 	const auto y = unique_id & 0xf;
-	float tmp_a[16];
 	float tmp_c[16 * 16 / warp_size];
 
-	for (auto i = 0; i < 16; i++){
-		tmp_a[i] = a[y + ldm_a * i];
-	}
-
-	for (auto i = 0; i < 16; i += 2){
+	for (auto i = 0; i < 16; i += 2) {
 		const auto x = i + lane;
 		float sum_ab = 0.0f;
-		float sum_dab = 0.0f;
-		float sum_adb = 0.0f;
-		for(unsigned k = 0; k < 16; k += 1){
-			const auto a_v = cutf::experimental::tf32::to_tf32(tmp_a[k]);
-			const auto b_v = cutf::experimental::tf32::to_tf32(b[x * ldm_b + k]);
+		for (unsigned k = 0; k < 32; k += 1) {
+			const auto a_v = cutf::type::cast<nvcuda::wmma::precision::tf32>(a[y + ldm_a * k]);
+			const auto b_v = cutf::type::cast<nvcuda::wmma::precision::tf32>(b[x * ldm_b + k]);
 			sum_ab = fmaf(a_v, b_v, sum_ab);
 		}
-		tmp_c[i / 2] += sum_adb + sum_dab + sum_ab;
+		tmp_c[i / 2] = sum_ab;
 	}
 
-	for(auto i = 0; i < 16; i += 2){
+	__syncthreads();
+	for (auto i = 0; i < 16; i += 2) {
 		const auto x = i + lane;
 		c[x * ldm_c + y] = tmp_c[i / 2];
 	}
