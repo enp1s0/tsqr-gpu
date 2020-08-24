3966bb1b0749c55d0e7911ab5a3f738e2ee91cf6
mutsuki <mutsuki@momo86.net>
2020-08-24 11:45:41 +0900

    Add the declaration of `mtk::validation::exponent_distribution`

--- src/validation.hpp
+++ src/validation.hpp
@@ -10,6 +10,8 @@ template <class T>
 void check_submatrix_orthogonality(const T* const matrix, const std::size_t m, const unsigned n);
 template <class T>
 void multi_orthogonality(const T* const ptr, const std::size_t m, const std::size_t n, const std::size_t ldm, const std::size_t size, cudaStream_t stream = 0);
+template <class T>
+void exponent_distribution(const T* const ptr, const std::size_t size, const char* const csv_item_name, cudaStream stream = 0);
 } // namespace validation
 } // namespace mtk
 
