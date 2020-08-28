942d16c163a1a4e6a3e290da1a815f46e30f9253
mutsuki <mutsuki@momo86.net>
2020-08-28 12:06:57 +0900

    Add the definition of `mtk::experimental::force_underflow`

---
+++ src/experimental.cu
@@ -0,0 +1,22 @@
+#include <cutf/experimental/exponent.hpp>
+#include "experimental.hpp"
+
+namespace {
+template <class T>
+__global__ void force_experimental_kernel(T* const ptr, const int min_exponent, const std::size_t size) {
+	const auto tid = blockIdx.x * blockDim.x + threadIdx.x;
+	if (tid >= size) return;
+
+	ptr[tid] = cutf::experimental::exponent::force_underflow<T>(ptr[tid], min_exponent);
+}
+} // noname namespace
+
+template <class T>
+void mtk::experimental::force_exponent(T* const ptr, const int min_exponent, const std::size_t size, cudaStream_t cuda_stream) {
+	constexpr std::size_t block_size = 1lu << 8;
+	
+	force_experimental_kernel<T><<<(size + block_size - 1) / block_size, block_size, 0, cuda_stream>>>(ptr, min_exponent, size);
+}
+
+template <> void mtk::experimental::force_exponent<half >(half * const, const int, const std::size_t, cudaStream_t);
+template <> void mtk::experimental::force_exponent<float>(float* const, const int, const std::size_t, cudaStream_t);
