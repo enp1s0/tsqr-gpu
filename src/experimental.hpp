bd9fcde32ae4f044a23c6a7ed8bf79c1e8b9b3ab
mutsuki <mutsuki@momo86.net>
2020-08-28 11:44:18 +0900

    Add the declaration of `experimental::force_underflow`

---
+++ src/experimental.hpp
@@ -0,0 +1,10 @@
+#ifndef __TSQR_EXPERIMENTAL_HPP__
+#define __TSQR_EXPERIMENTAL_HPP__
+
+namespace mtk {
+namespace experimental {
+template <int min_exponent, class T>
+void force_exponent(T* const ptr, const std::size_t size);
+} // namespace experimental
+} // namespace mtk
+#endif
