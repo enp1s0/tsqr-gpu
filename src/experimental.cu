9bbad102aa698f44cf664b28fdf4b3b4efad4d03
mutsuki <mutsuki@momo86.net>
2020-08-29 11:14:20 +0900

    Rename `mtk::experimental::force_exponent` to `mtk::experimental::min_exponent`

--- src/experimental.cu
+++ src/experimental.cu
@@ -3,7 +3,7 @@
 
 namespace {
 template <class T>
-__global__ void force_experimental_kernel(T* const ptr, const int min_exponent, const std::size_t size) {
+__global__ void min_exponent_kernel(T* const ptr, const int min_exponent, const std::size_t size) {
 	const auto tid = blockIdx.x * blockDim.x + threadIdx.x;
 	if (tid >= size) return;
 
@@ -12,11 +12,11 @@ __global__ void force_experimental_kernel(T* const ptr, const int min_exponent, 
 } // noname namespace
 
 template <class T>
-void mtk::experimental::force_exponent(T* const ptr, const int min_exponent, const std::size_t size, cudaStream_t const cuda_stream) {
+void mtk::experimental::min_exponent(T* const ptr, const int min_exponent, const std::size_t size, cudaStream_t const cuda_stream) {
 	constexpr std::size_t block_size = 1lu << 8;
 	
-	force_experimental_kernel<T><<<(size + block_size - 1) / block_size, block_size, 0, cuda_stream>>>(ptr, min_exponent, size);
+	min_exponent_kernel<T><<<(size + block_size - 1) / block_size, block_size, 0, cuda_stream>>>(ptr, min_exponent, size);
 }
 
-template void mtk::experimental::force_exponent<half >(half * const, const int, const std::size_t, cudaStream_t const);
-template void mtk::experimental::force_exponent<float>(float* const, const int, const std::size_t, cudaStream_t const);
+template void mtk::experimental::min_exponent<half >(half * const, const int, const std::size_t, cudaStream_t const);
+template void mtk::experimental::min_exponent<float>(float* const, const int, const std::size_t, cudaStream_t const);
