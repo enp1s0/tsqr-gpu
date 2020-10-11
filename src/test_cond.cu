eccb3d51413452afb5223f45318c1578ee1020c9
mutsuki <mutsuki@momo86.net>
2020-10-11 23:03:11 +0900

    Fix csv colmun names in `print_accuracy_head`

--- src/test_cond.cu
+++ src/test_cond.cu
@@ -107,7 +107,7 @@ __global__ void make_zero(DST_T* const dst, const std::size_t size){
 }
 
 void print_accuracy_head() {
-	std::cout << "m,n,cond,type,core_type,tc,correction,reorthogonalization,error,error_deviation,orthogonality,orthogonality_deviation" << std::endl;
+	std::cout << "m,n,cond,type,compute_mode,reorthogonalization,residual,residual_deviation,orthogonality,orthogonality_deviation" << std::endl;
 	std::cout.flush();
 }
 
