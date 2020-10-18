b4c88497afa61697e0725e2b6820cf99f915b2c5
mutsuki <mutsuki@momo86.net>
2020-10-18 13:52:07 +0900

    Update submodule runtime_status

--- src/runtime-status/test/hip_status.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <runtime_staus/hip.hpp>
-
-constexpr bool print_header = true;
-
-int main() {
-	mtk::runtime_status::hip::print_info(print_header);
-	mtk::runtime_status::hip::print_current_device_id(print_header);
-}
